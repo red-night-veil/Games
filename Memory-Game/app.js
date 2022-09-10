f3638eacc573de6646211265711f4ea894b8f6f9
red-night-veil <lorisaluna99@gmail.com>
2022-09-09 19:56:56 -0500

    Creating the board and linking array of cards

--- Memory-Game/app.js
+++ Memory-Game/app.js
@@ -33,3 +33,18 @@ const cards= [
     }
 ]
 
+cards.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for(let i = 0; i < 10; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.append(card)
+    }
+}
+createBoard()
+
+
